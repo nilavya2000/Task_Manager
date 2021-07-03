a5c25f0b0def6342a80494f95a7853a2babfb3f3
nilavya2000 <dnilavya@gmail.com>
2021-07-03 23:47:26 +0530

    updated_the_ui

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.today)
 
     def __repr__(self):
         return '<Task %r>' %self.id
